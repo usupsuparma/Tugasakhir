e0ba5b9b6d1fd3bbd74bbc0794492cf23751e0b4
usupsuparma <usup.suparma7@gmail.com>
2018-08-04 15:34:45 +0000

    iada jarak

---
+++ jarak/jarak.ino
@@ -0,0 +1,31 @@
+int pTrig = 11;
+int pEcho = 4;
+long durasi;
+int percobaan= 1;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(pTrig, OUTPUT);
+  pinMode(pEcho, INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  int jarak = trig();
+  Serial.println(jarak);
+  delay(1000);
+}
+
+long trig(){
+  digitalWrite(pTrig, LOW);
+  delayMicroseconds(1000);
+  digitalWrite(pTrig, HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(pTrig, LOW);
+  durasi = pulseIn(pEcho, HIGH);
+  return durasi / 29 / 2;
+}
+
