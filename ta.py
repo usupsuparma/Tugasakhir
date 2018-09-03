71e61f5dd643a9b2658a5556f9a5175c7894dd81
usupsuparma <usup.suparma7@gmail.com>
2018-09-03 19:33:25 +0700

    update lagi

--- ta.py
+++ ta.py
@@ -66,6 +66,47 @@ class ObjectDetection():
 
 # ======== Akhir kelas objek detection==================
 
+#===== deteksi Lubang
+class Lubang():
+	__image = "test.jpg"
+
+	def __init__(self,img):
+		self.__image = img
+
+	def identifikasi(self):
+		gray = cv2.cvtColor(self.__image, cv2.COLOR_BGR2GRAY)
+		gray = cv2.GaussianBlur(gray, (5, 5), 0)
+
+		# threshold the image, then perform a series of erosions +
+		# dilations to remove any small regions of noise
+		thresh = cv2.threshold(gray, 45, 255, cv2.THRESH_BINARY_INV)[1]
+		thresh = cv2.erode(thresh, None, iterations=2)
+		thresh = cv2.dilate(thresh, None, iterations=2)
+		cv2.imshow("Image",thresh)
+
+		# find contours in thresholded image, then grab the largest one
+
+		cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+			cv2.CHAIN_APPROX_SIMPLE)
+		cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+		c = max(cnts, key=cv2.contourArea)
+
+		# determine the most extreme points along the contour
+		extLeft = tuple(c[c[:, :, 0].argmin()][0])
+		extRight = tuple(c[c[:, :, 0].argmax()][0])
+		extTop = tuple(c[c[:, :, 1].argmin()][0])
+		extBot = tuple(c[c[:, :, 1].argmax()][0])
+
+		# draw the outline of the object, then draw each of the
+		# extreme points, where the left-most is red, right-most
+		# is green, top-most is blue, and bottom-most is teal
+		cv2.drawContours(image, [c], -1, (0, 255, 255), 2)
+		cv2.circle(image, extLeft, 6, (0, 0, 255), -1)
+		cv2.circle(image, extRight, 6, (0, 255, 0), -1)
+		cv2.circle(image, extTop, 6, (255, 0, 0), -1)
+		cv2.circle(image, extBot, 6, (255, 255, 0), -1)
+
+
 
 #=========== ini adalah main program =============
 print("=="*10)
@@ -165,6 +206,10 @@ while True:
 						mixer.music.load('musik/kursi.mp3')
 						mixer.music.play()
 						time.sleep(7)
+					elif "none" is result:
+						print("identifikasi Lubang")
+
+
 
 					print("hasil uji: ",result)
 
